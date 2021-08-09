4e0d87ecba9db767a887002dacafa0b12e8edf2d
jsilva <jsilva@grupodot.com>
2021-08-09 12:17:13 -0500

    User and Role Entities created. Close #2

--- Aplicacion/src/main/java/com/sebastianjpaspringboot/app/AplicacionApplication.java
+++ Aplicacion/src/main/java/com/sebastianjpaspringboot/app/AplicacionApplication.java
@@ -1,4 +1,4 @@
-package com.sebastianjpaspringboot.aplicacion;
+package com.sebastianjpaspringboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
